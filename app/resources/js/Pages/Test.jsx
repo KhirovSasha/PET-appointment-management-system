ddb4b75ee11bc5c120517d895de38114ba9969db
Sasha Khirov <sasha.khiro@gmail.com>
2023-12-02 01:12:11 +0200

    implementing laravel project

---
+++ app/resources/js/Pages/Test.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import YourComponent from "./YourComponent.jsx";
+
+const Test = () => {
+    return (
+        <>
+            <h1>This is test component</h1>
+            <YourComponent />
+        </>
+    )
+}
+
+export default Test
---
+++ app/resources/js/Pages/YourComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const YourComponent = ({ data }) => {
+    return (
+        <div>
+            {data}
+
+        </div>
+    );
+};
+
+export default YourComponent;
