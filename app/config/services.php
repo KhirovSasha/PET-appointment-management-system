7583595d3d00c1829f83e6049a377942570b2da9
Sasha Khirov <sasha.khiro@gmail.com>
2023-12-03 15:18:34 +0200

    adding laravel breeze for auth in app

--- app/config/services.php
+++ app/config/services.php
@@ -31,4 +31,10 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'google' => [
+        'client_id' => env('GOOGLE_AUTH_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_AUTH_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_REDIRECT_URL'),
+    ]
+
 ];
