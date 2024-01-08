d99b741522ab5a1a29383a79bc5b7f655729adf7
KhirovSasha <sasha.khiro@gmail.com>
2024-01-08 02:53:48 +0200

    Added link to page Calendar

--- app/resources/js/Layouts/AuthenticatedLayout.jsx
+++ app/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('calendar')} active={route().current('calendar')}>
+                                    Calendar
+                                </NavLink>
                             </div>
                         </div>
 
